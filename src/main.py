d53f964c872356c377b532d41a8e5c069744127e
denis68182 <122289473+denis68182@users.noreply.github.com>
2023-01-14 21:11:22 -0500

    create the main program

---
+++ src/main.py
@@ -0,0 +1,5 @@
+
+#How to make it?
+file = open("res/myfile.txt","w")
+file.write(" Lets make it in TZ. You will enjoy Mountain Kilimanjaro")
+file.close()
---
+++ src/main2.py
@@ -0,0 +1,11 @@
+# this is subprogram  2 run by main2.py stored in src
+
+with open("res/main2.txt","w") as f:
+	f.write("Okay")
+
+
+	x =2,
+	y = 3,
+	z = x + y,
+	print("The value of z is", z)
+
