b032260acdd8cdf98d36503d309f2cab54f1c4b8
Denis Kiobia <denis.kiobia@uga.edu>
2023-01-15 00:01:57 -0500

    new updates

--- src/main.py
+++ src/main.py
@@ -4,10 +4,13 @@
 import time
 import  os
 
-num_files = len(os.listdir("data"))   #data  is the data folder now will appear in the fille  git folders to be commited after having data (number of files)
+num_files = len(os.listdir("data"))   #list number of  files/folders in the data folder
+num_files2 = len(os.listdir("src"))
 file = open("res/myfile.txt","w")
 file.write(f"The program was just run at {time.ctime()}\n")
-file.write(" Lets make it in TZ. You will enjoy Mountain Kilimanjaro. \n")
-file.write("Dont forget to visit MOrogoro, Bigwa Stenda. \n")
-file.write(f"There are {num_files} in the data folder")
+file.write(" Lets make it in TZ. You will enjoy Mountain Kilimanjaro.\n")
+file.write("Dont forget to visit MOrogoro, Bigwa Stenda.\n")
+file.write(f"There are {num_files} in the data folder\n")
+file.write(f"There are {num_files2} in the source/program code folder \n")
+file.write("Good")
 file.close()
