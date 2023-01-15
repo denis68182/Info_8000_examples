101528dd5b5ff6699a53489a6156c624862f544a
Denis Kiobia <denis.kiobia@uga.edu>
2023-01-14 22:24:15 -0500

    main.py updated again

--- src/main.py
+++ src/main.py
@@ -2,5 +2,5 @@
 #How to make it?
 file = open("res/myfile.txt","w")
 file.write(" Lets make it in TZ. You will enjoy Mountain Kilimanjaro")
-file.write("Dont forget to visit MOrogoro, Bigwa")
+file.write("Dont forget to visit MOrogoro, Bigwa Stenda")
 file.close()
